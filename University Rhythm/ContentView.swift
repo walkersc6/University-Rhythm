1269b254e8dde09daa20f154e8082bfd2738dd9b
Sarah Walker <walkersc6>
2025-10-17 19:14:14 -0600

    Initial Commit

---
+++ University Rhythm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  University Rhythm
+//
+//  Created by Sarah Walker on 10/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ University Rhythm/University_RhythmApp.swift
@@ -0,0 +1,17 @@
+//
+//  University_RhythmApp.swift
+//  University Rhythm
+//
+//  Created by Sarah Walker on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct University_RhythmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

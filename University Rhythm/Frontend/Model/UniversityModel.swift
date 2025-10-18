3490ef813dd16172d514750522f063c06fd1837c
Sarah Walker <walkersc6>
2025-10-17 19:33:00 -0600

    init

---
+++ University Rhythm/Frontend/Model/UniversityModel.swift
@@ -0,0 +1,12 @@
+//
+//  UniversityModel.swift
+//  University Rhythm
+//
+//  Created by Sarah Walker on 10/17/25.
+//
+
+import Foundation
+
+struct UniversityModel: Codable {
+
+}
---
+++ University Rhythm/Frontend/ViewModel/UniversityViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  UniversityViewModel.swift
+//  University Rhythm
+//
+//  Created by Sarah Walker on 10/17/25.
+//
+
+import Foundation
+
+@Observable class UniversityViewModel:  {
+    
+}
